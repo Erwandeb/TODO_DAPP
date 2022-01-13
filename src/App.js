5998a1a227871389f826f32aaa8a16815dada376
Erwan de Belleville <edebelleville@hotmail.fr>
2022-01-13 08:51:05 +0100

    add dependencies

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import './style/index.scss'
 import Home from './pages/Home';
+import {useState, useEffect} from 'react'
+import Todolist from './build/contracts/Todolist.json'
+
 
 function App() {
   return (
